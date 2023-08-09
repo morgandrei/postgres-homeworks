1596f8131c6025308f1167539a788d34facf9322
Andrey <moregun@list.ru>
2023-08-09 13:33:19 +0300

    Домашка 3

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -1,24 +1,20 @@
 -- Напишите запросы, которые выводят следующую информацию:
 -- 1. Название компании заказчика (company_name из табл. customers) и ФИО сотрудника, работающего над заказом этой компании (см таблицу employees),
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
-
+SELECT customers.company_name AS customer, CONCAT(employees.first_name, ' ', employees.last_name) AS employee FROM orders
+INNER JOIN employees USING(employee_id)
+INNER JOIN shippers ON orders.ship_via=shippers.shipper_id
+INNER JOIN customers USING(customer_id)
+WHERE customers.city=employees.city AND customers.city='London' AND shippers.company_name='United Package'
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
-"""
-SELECT product_name, units_in_stock, suppliers.contact_name, suppliers.phone
-FROM products
-INNER JOIN categories USING(category_id) WHERE category_name = ('Dairy Products', 'Condiments')
---INNER JOIN suppliers USING(supplier_id)  WHERE discontinued = 0 AND units_in_stock < 25
-
+SELECT product_name, units_in_stock, contact_name, phone FROM products
+INNER JOIN categories USING(category_id)
+INNER JOIN suppliers USING(supplier_id) WHERE discontinued = 0 AND units_in_stock < 25 AND category_name IN ('Dairy Products', 'Condiments')
 ORDER BY units_in_stock
-"""
-
-
-
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 SELECT company_name FROM customers
